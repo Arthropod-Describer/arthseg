b5c91bb72bf213a339cdd61a483ae5878d562426
Matej Pekar <matej.pekar120@gmail.com>
2022-11-09 21:26:04 +0100

    debug

--- arthseg/module.cpp
+++ arthseg/module.cpp
@@ -8,6 +8,8 @@
 #include "src/mask/remove_dirt.hpp"
 #include "src/regions/refine.hpp"
 
+#include <iostream>
+
 static PyObject *Py_RemoveDirt(PyObject *, PyObject *args, PyObject *kwargs)
 {
     PyArrayObject *image;
@@ -25,6 +27,7 @@ static PyObject *Py_RemoveDirt(PyObject *, PyObject *args, PyObject *kwargs)
 
 static PyObject *Py_FillHoles(PyObject *, PyObject *args, PyObject *kwargs)
 {
+    std::cout << "called fill holes" << std::endl;
     PyArrayObject *image;
     float hole_area = 0.001;
     const char *kwlist[] = { "", "hole_area", NULL };
@@ -32,6 +35,8 @@ static PyObject *Py_FillHoles(PyObject *, PyObject *args, PyObject *kwargs)
         PyErr_SetString(PyExc_TypeError, "Invalid argumnets");
         return NULL;
     }
+
+    std::cout << "loaded args" << std::endl;
 
     return Py_BuildValue("O", fill_holes(image, hole_area));
 }
--- arthseg/src/mask/fill_holes.cpp
+++ arthseg/src/mask/fill_holes.cpp
@@ -4,8 +4,11 @@
 #include "fill_holes.hpp"
 #include "utils.hpp"
 
+#include <iostream>
+
 PyArrayObject *fill_holes(PyArrayObject *image, float hole_area)
 {
+    std::cout << "fill holes function" << std::endl;
     import_array();
     PyArrayObject *mask = (PyArrayObject *) PyArray_EMPTY(PyArray_NDIM(image), PyArray_DIMS(image), NPY_UINT8, 0);
     PyArrayObject *output = (PyArrayObject *) PyArray_Empty(PyArray_NDIM(image), PyArray_DIMS(image), PyArray_DTYPE(image), 0);
@@ -14,6 +17,8 @@ PyArrayObject *fill_holes(PyArrayObject *image, float hole_area)
         return NULL;
     }
 
+    std::cout << "created arrays" << std::endl;
+
     for (npy_intp row = 0; row < PyArray_DIM(image, 0); row++) {
         for (npy_intp col = 0; col < PyArray_DIM(image, 1); col++) {
             auto value = PyLong_AsUnsignedLong(PyArray_GETITEM(image, (char *) PyArray_GETPTR2(image, row, col)));
@@ -21,17 +26,27 @@ PyArrayObject *fill_holes(PyArrayObject *image, float hole_area)
         }
     }
 
+    std::cout << "copied" << std::endl;
+
     auto components = connected_components(mask, CONNECTIVITY_4);
+
+    std::cout << "connected components runed" << std::endl;
     auto area = std::accumulate(components.begin(), components.end(), 0, [](auto acc, auto &component) {
         return acc + component.size();
     });
+
+    std::cout << "calcualted area" << std::endl;
 
     if (PyArray_CopyInto(output, image)) {
         PyErr_SetString(PyExc_RuntimeError, "Failed to copy image");
         return NULL;
     }
 
+    std::cout << "copying image" << std::endl;
+
     size_t max_area = hole_area * (PyArray_DIM(image, 0) * PyArray_DIM(image, 1) - area);
+
+    std::cout << "got max area" << std::endl;
     for (auto &component : components) {
         if (component.size() < max_area) {
             for (auto &node : component.nodes) {
@@ -39,6 +54,8 @@ PyArrayObject *fill_holes(PyArrayObject *image, float hole_area)
             }
         }
     }
+
+    std::cout << "cleaned" << std::endl;
 
     return output;
 }
